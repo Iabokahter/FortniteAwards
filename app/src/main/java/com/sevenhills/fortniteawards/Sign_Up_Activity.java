d5d44ab0a4fd50ed409f820f19a8c8e5229fd9fd
Mr <waseemhijazi@yahoo.com>
2018-12-04 22:24:34 +0200

    Two Layout

---
+++ app/src/main/java/com/sevenhills/fortniteawards/Sign_Up_Activity.java
@@ -0,0 +1,25 @@
+package com.sevenhills.fortniteawards;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Button;
+
+public class Sign_Up_Activity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.sign_up_layout);
+
+
+        Button next = (Button) findViewById(R.id.continue_button);
+        next.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View view) {
+                Intent myIntent = new Intent(view.getContext(), MainActivity.class);
+                startActivityForResult(myIntent, 0);
+            }
+        });
+    }
+}
